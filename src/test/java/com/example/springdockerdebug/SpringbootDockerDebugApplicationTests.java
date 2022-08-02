b4dddb96758cd123bff1454828e9eb3aba6a390f
Tiraj KC <kctiraj44@gmail.com>
2022-08-02 18:33:03 -0500

    Added all files

---
+++ src/test/java/com/example/springdockerdebug/SpringbootDockerDebugApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdockerdebug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDockerDebugApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
